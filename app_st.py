4e1d4117c272a14ac096a1303c63921238585bdc
OSDev (databox) <osvel.glezval@gmail.com>
2023-10-31 12:18:15 -0400

    Update app_st.py

--- app_st.py
+++ app_st.py
@@ -16,7 +16,6 @@ c = conn.cursor()
 #''')
 #conn.commit()
 
-
 # Función para insertar una persona en la base de datos
 def insertar_persona(nombre, edad):
     c.execute("INSERT INTO personas (nombre, edad) VALUES (?, ?)", (nombre, edad))
